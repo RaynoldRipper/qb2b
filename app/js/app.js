c8f605ee09796829ae0df3050507c59ffeca1145
ReynoldRipper <Raynoldripper@gmail.com>
2020-10-20 01:33:21 +0300

    tarif

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,19 @@
-let hamburger = document.querySelector('.hamburger');
-let menu = document.querySelector('.bgr-wrapper');
+// let hamburger = document.querySelector('.hamburger');
+// let menu = document.querySelector('.bgr-wrapper');
 
-function hambToggle(){
-  if (hamburger.classList.contains('active')){
-    hamburger.classList.remove('active');
-  }
-  else {
-    hamburger.classList.add('active');
-  }
-}
+// function hambToggle(){
+//   if (hamburger.classList.contains('active')){
+//     hamburger.classList.remove('active');
+//   }
+//   else {
+//     hamburger.classList.add('active');
+//   }
+// }
 
-hamburger.addEventListener('click', hambToggle);
+// hamburger.addEventListener('click', hambToggle);
+
+$( document ).ready(function() {
+  var h = $('.tarif__table').css('height');
+  console.log(h);
+  $('head').append('<style>.table-item::before { height: ' + h + '; }</style>');  
+});

0bdd5389ce500bdc24cb430f50edb6d5d6d100e5
ReynoldRipper <Raynoldripper@gmail.com>
2020-10-21 01:25:00 +0300

    rdy

--- app/js/app.js
+++ app/js/app.js
@@ -14,16 +14,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var h = $('.table-content').css('height');
   console.log(h);
   $('head').append('<style>.table-item::before { height: ' + h + '; }</style>');  
-
-  var wow = new WOW(
-    {
-      boxClass:     'section-info',      // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
-      animateClass: 'animated', // класс для анимации элемента (по умолчанию, animated)
-      offset:       0,          // расстояние в пикселях от нижнего края браузера до верхней границы элемента, необходимое для начала анимации (по умолчанию, 0)
-      mobile:       true,       // включение/отключение WOW.js на мобильных устройствах (по умолчанию, включено)
-      live:         true,       // поддержка асинхронно загруженных элементов (по умолчанию, включена)
-      scrollContainer: null // селектор прокручивающегося контейнера (опционально, по умолчанию, window)
-    }
-  );
-  wow.init();
+  
 });

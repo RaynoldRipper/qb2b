ca122570612f58f9a88a1e4edc74384a187d47c2
Ильяс <gikami.book@gmail.com>
2020-10-20 18:18:29 +0300

    work

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,29 @@
-// let hamburger = document.querySelector('.hamburger');
-// let menu = document.querySelector('.bgr-wrapper');
+document.addEventListener("DOMContentLoaded", function(event) {
+  let tableOpenBtn = document.querySelector('.btn-open');
+  let tableWrapper = document.querySelector('.table-wrapper');
+  let burgerButton = document.querySelector('.burger-menu');
 
-// function hambToggle(){
-//   if (hamburger.classList.contains('active')){
-//     hamburger.classList.remove('active');
-//   }
-//   else {
-//     hamburger.classList.add('active');
-//   }
-// }
+  tableOpenBtn.addEventListener('click',function(){
+    tableWrapper.classList.toggle('table-wrapper--opened');
+    tableOpenBtn.classList.toggle('btn-open--active');
+  });
+  burgerButton.addEventListener('click', function(){
+    burgerButton.classList.toggle('burger-menu--active');
+  });
 
-// hamburger.addEventListener('click', hambToggle);
-
-$( document ).ready(function() {
-  var h = $('.tarif__table').css('height');
+  var h = $('.table-content').css('height');
   console.log(h);
   $('head').append('<style>.table-item::before { height: ' + h + '; }</style>');  
+
+  var wow = new WOW(
+    {
+      boxClass:     'section-info',      // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
+      animateClass: 'animated', // класс для анимации элемента (по умолчанию, animated)
+      offset:       0,          // расстояние в пикселях от нижнего края браузера до верхней границы элемента, необходимое для начала анимации (по умолчанию, 0)
+      mobile:       true,       // включение/отключение WOW.js на мобильных устройствах (по умолчанию, включено)
+      live:         true,       // поддержка асинхронно загруженных элементов (по умолчанию, включена)
+      scrollContainer: null // селектор прокручивающегося контейнера (опционально, по умолчанию, window)
+    }
+  );
+  wow.init();
 });
